d6251dce322f10b1beeb476b19b6f027781acd92
黃祥璽 <436frank@gmail.com>
2022-10-17 18:18:20 +0800

    測IR 讀取時間

--- src/Init_car_index.h
+++ src/Init_car_index.h
@@ -20,6 +20,8 @@
 #define Buzzer_PIN 4
 #define Button_PIN 5
 /** LED **/
+#define ON  LOW
+#define OFF HIGH
 #define LED_R_PIN 14
 #define LED_L_PIN 13
 #define LED_1_PIN 30
@@ -37,6 +39,14 @@
 #define IR5 A5
 #define IR_hint_L A2
 #define IR_StartStop_R A6
+
+unsigned int cCount=0;    // calibration count
+unsigned char IRindex[7] = {A1, A2, A0, A3, A4, A5, A6};
+int IRsensors[7]={0}, IR_caliValues[7];
+int IR_vMax[7]={0, 0, 0, 0, 0, 0,0}, IR_vMin[7]={900, 900, 900, 900, 900, 900,900};
+int16_t start_cont=0;
+// Weighted average
+int Lp;
 /**  **/
 void Init_Peripherals();
 
@@ -51,6 +61,12 @@ void Init_Peripherals()
     pinMode(LED_2_PIN , OUTPUT);
     pinMode(LED_3_PIN , OUTPUT);
     pinMode(LED_L_PIN , OUTPUT); pinMode(LED_R_PIN, OUTPUT);
+    digitalWrite(LED_1_PIN,OFF);
+    digitalWrite(LED_2_PIN,OFF);
+    digitalWrite(LED_3_PIN,OFF);
+    digitalWrite(LED_L_PIN,OFF);
+    digitalWrite(LED_R_PIN,OFF);
+
     /** Init MOTOR **/
     pinMode(MOTOR_DIR_L, OUTPUT);pinMode(MOTOR_PWM_L, OUTPUT);
     pinMode(MOTOR_DIR_R, OUTPUT);pinMode(MOTOR_PWM_R, OUTPUT);
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
 #include <Init_car_index.h>
-void setup() {
+void setup()
+{
     Init_Peripherals();
+    SerialUSB.begin(115200);
 }
 
-void loop() {
-// write your code here
+void loop()
+{
+    digitalWrite(Buzzer_PIN,ON);
+    digitalWrite(IR3_OUT,HIGH);
+    IRsensors[3]=analogRead(IR3);
+    digitalWrite(IR3_OUT,LOW);
+    digitalWrite(IR0_4_OUT,HIGH);
+    IRsensors[0]=analogRead(IR_hint_L);
+    IRsensors[4]=analogRead(IR4);
+    digitalWrite(IR0_4_OUT,LOW);
+    digitalWrite(IR2_6_OUT,HIGH);
+    IRsensors[6]=analogRead(IR_StartStop_R);
+    IRsensors[2]=analogRead(IR2);
+    digitalWrite(IR2_6_OUT,LOW);
+    digitalWrite(IR1_5_OUT,HIGH);
+    IRsensors[1]=analogRead(IR1);
+    IRsensors[5]=analogRead(IR5);
+    digitalWrite(IR1_5_OUT,LOW);
+    digitalWrite(Buzzer_PIN,OFF);
+    for (int i = 0; i < 7; ++i) {
+    SerialUSB.print(IRsensors[i]);
+    SerialUSB.print("\t");
+    }
+    SerialUSB.println();
 }
