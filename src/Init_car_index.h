b61549962ca256fc8f9207fc2c3b4a8e6e2a00f9
黃祥璽 <436frank@gmail.com>
2022-10-18 17:20:17 +0800

    測qei

--- src/Init_car_index.h
+++ src/Init_car_index.h
@@ -19,6 +19,7 @@
 /** Buzzer button **/
 #define Buzzer_PIN 4
 #define Button_PIN 5
+#define countButton 5
 /** LED **/
 #define ON  LOW
 #define OFF HIGH
@@ -39,7 +40,9 @@
 #define IR5 A5
 #define IR_hint_L A2
 #define IR_StartStop_R A6
-
+#define IRmin         5
+#define IRmax         4000
+/** IR variable **/
 unsigned int cCount=0;    // calibration count
 unsigned char IRindex[7] = {A1, A2, A0, A3, A4, A5, A6};
 int IRsensors[7]={0}, IR_caliValues[7];
@@ -47,14 +50,171 @@ int IR_vMax[7]={0, 0, 0, 0, 0, 0,0}, IR_vMin[7]={900, 900, 900, 900, 900, 900,90
 int16_t start_cont=0;
 // Weighted average
 int Lp;
-/**  **/
-void Init_Peripherals();
+/** Encoder variable **/
+struct Motion_status {
+    float  pOLD;
+    float  pNEW;
+    float  pERROR;
+    float  vOLD;
+    float  vNEW;
+};
+Motion_status eMotionR, eMotionL;
+float EstR_acceleration = 0, EstL_acceleration;
+volatile float count_L=0, count_R=0, Pcount_L=0, Pcount_R=0;  // initialize variables
+volatile float  Vcount_L=0, Vcount_R=0;
+float pos_now=0; //現在的pos位置
+float pos_old=0; //現在的pos位置
+float velocity = 0;
+/** Button variable **/
+boolean button, buttonPressed;
+unsigned char sButton = 0;  // status of Button
+unsigned int bPressCount = 0, bReleaseCount=0;
 
+/**  **/
+
+void readAllIR_values();
+void IR_calibrations();
+void IR_Max_Min();
+
+void Encoder_LA();
+void Encoder_LB();
+void Encoder_RA();
+void Encoder_RB();
+
+void Init_Peripherals();
+void AdcBooster();
+void setupTimers();
+void setupPWM();
+
+/** Encoder() **/
+void READ_QEI() {
+    // calulate the speed information of the wheel,  unit:pulses/T_interval
+    Vcount_R = count_R - Pcount_R;
+    Vcount_L = count_L - Pcount_L;
+    // keep previous pulse counts
+    Pcount_R = count_R;
+    Pcount_L = count_L;
+
+}
+void QEI_filter() {
+    pos_now=((eMotionL.pNEW + eMotionR.pNEW)/2);  //pos 左右相加/2
+    velocity=((eMotionL.vNEW + eMotionR.vNEW)/2);  //pos 左右相加/2
+    // wn = 0.1, zeta = 1, Kp = wn^2 = 0.01, Kv = 2*zeta*wn = 0.2
+    // update for velocity estimation
+    eMotionR.vNEW = eMotionR.vOLD + EstR_acceleration;
+    eMotionL.vNEW = eMotionL.vOLD + EstL_acceleration;
+    // update for position estimation
+    eMotionR.pNEW = eMotionR.pOLD + eMotionR.vOLD;
+    eMotionL.pNEW = eMotionL.pOLD + eMotionL.vOLD;
+    // update for estimation error
+    eMotionR.pERROR = Pcount_R - eMotionR.pNEW;
+    eMotionL.pERROR = Pcount_L - eMotionL.pNEW;
+    // update for acceleration estimation
+    EstR_acceleration = eMotionR.pERROR / 100.0 - eMotionR.vNEW / 5.0; //pError*Kp - vNew*Kv
+    EstL_acceleration = eMotionL.pERROR / 100.0 - eMotionL.vNEW / 5.0; //pError*Kp - vNew*Kv
+    // update OLD estimation
+    eMotionR.vOLD = eMotionR.vNEW;
+    eMotionR.pOLD = eMotionR.pNEW;
+    eMotionL.vOLD = eMotionL.vNEW;
+    eMotionL.pOLD = eMotionL.pNEW;
+}
+void Encoder_LA() {
+    if (digitalRead(Encoder_L_A)==digitalRead(Encoder_L_B)) {count_L--;}
+    else {count_L++;}
+}
+void Encoder_LB() {
+    if (digitalRead(Encoder_L_A)==digitalRead(Encoder_L_B)) {count_L++;}
+    else {count_L--;}
+}
+void Encoder_RA() {
+    if (digitalRead(Encoder_R_A)==digitalRead(Encoder_R_B)) {count_R--;}
+    else {count_R++;}
+}
+void Encoder_RB() {
+    if (digitalRead(Encoder_R_A)==digitalRead(Encoder_R_B)) {count_R++;}
+    else {count_R--;}
+}
+/** IR() **/
+void readAllIR_values() {
+    digitalWrite(IR3_OUT,HIGH);
+    IRsensors[3]=analogRead(IR3);
+    digitalWrite(IR3_OUT,LOW);
+    digitalWrite(IR0_4_OUT,HIGH);
+    IRsensors[0]=analogRead(IR_hint_L);
+    IRsensors[4]=analogRead(IR4);
+    digitalWrite(IR0_4_OUT,LOW);
+    digitalWrite(IR2_6_OUT,HIGH);
+    IRsensors[6]=analogRead(IR_StartStop_R);
+    IRsensors[2]=analogRead(IR2);
+    digitalWrite(IR2_6_OUT,LOW);
+    digitalWrite(IR1_5_OUT,HIGH);
+    IRsensors[1]=analogRead(IR1);
+    IRsensors[5]=analogRead(IR5);
+    digitalWrite(IR1_5_OUT,LOW);
+}
+void IR_calibrations() {
+    unsigned char index;
+    long temp;
+    for (index = 1; index < 6; index++) {
+        temp = long(IRmax - IRmin) * long(IRsensors[index] - IR_vMin[index]);
+        IR_caliValues[index] = IRmin + temp / (IR_vMax[index] - IR_vMin[index]);
+        if (IR_caliValues[index] < 0)
+            IR_caliValues[index] = 0;
+    }
+}
+void IR_Max_Min() {
+    unsigned char index;
+    for (index = 1; index < 6; index++) {
+        if (IR_vMax[index] < IRsensors[index])
+            IR_vMax[index] = IRsensors[index];
+        if (IR_vMin[index] > IRsensors[index])
+            IR_vMin[index] = IRsensors[index];
+    }
+}
+/** Button() **/
+void checkButton() {
+    if (cCount < 1500) cCount++;
+    button = digitalRead(Button_PIN);
+    if (button == LOW) // button pressed
+    {
+        if (buttonPressed == LOW) // button pressed not yet confirmed
+        {
+            bPressCount++;
+            if (bPressCount > countButton) {
+                buttonPressed = HIGH;           // button pressed confirmed
+                bPressCount = 0;                // reset pressed count value
+            }
+        } else {                              // button pressed confirmed and button not yet released
+            bReleaseCount = 0;
+            bPressCount = 0;
+        }
+    } else // button==HIGH, released
+    {
+        if (buttonPressed == LOW) // button pressed not yet confirmed
+        {
+            bReleaseCount = 0;
+            bPressCount = 0;
+        } else                    // button pressed confirmed
+        {
+            bReleaseCount++;
+            if (bReleaseCount > countButton) {
+                buttonPressed = LOW;            // button released confirmed
+                bReleaseCount = 0;              // reset released count value
+                sButton++;                      // change Button status value
+                tone(Buzzer_PIN,1318,200);
+                if (sButton == 1) cCount = 0;     // reset
+                if (sButton > 5) sButton = 0;       // the largest value of Button status is 3
+            }
+        }
+    }
+}
+/** Init() **/
 void Init_Peripherals()
 {
+    /** Init ADC PWM ->12Bit **/
     analogReadResolution(12);   // ADC: 0~4095
     analogWriteResolution(12);  // PWM: 0~4095
-    /****/
+    /** Init Button Buzzer Led */
     pinMode(Button_PIN, INPUT);
     pinMode(Buzzer_PIN, OUTPUT);
     pinMode(LED_1_PIN , OUTPUT);
@@ -66,7 +226,6 @@ void Init_Peripherals()
     digitalWrite(LED_3_PIN,OFF);
     digitalWrite(LED_L_PIN,OFF);
     digitalWrite(LED_R_PIN,OFF);
-
     /** Init MOTOR **/
     pinMode(MOTOR_DIR_L, OUTPUT);pinMode(MOTOR_PWM_L, OUTPUT);
     pinMode(MOTOR_DIR_R, OUTPUT);pinMode(MOTOR_PWM_R, OUTPUT);
@@ -82,12 +241,105 @@ void Init_Peripherals()
     pinMode(IR5, INPUT);
     pinMode(IR_hint_L, INPUT);pinMode(IR_StartStop_R, INPUT);
     /** Init Encoder **/
-    // Left wheel encoder pins and interrupts/
-//    pinMode(Encoder_L_A, INPUT);  pinMode(Encoder_L_B, INPUT);
-//    attachInterrupt(digitalPinToInterrupt(Encoder_L_A), Encoder_LA, CHANGE);
-//    attachInterrupt(digitalPinToInterrupt(Encoder_L_B), Encoder_LB, CHANGE);
-//    // Right wheel encoder pins and interrupts
-//    pinMode(Encoder_R_A, INPUT);  pinMode(Encoder_R_B, INPUT);
-//    attachInterrupt(digitalPinToInterrupt(Encoder_R_A), Encoder_RA, CHANGE);
-//    attachInterrupt(digitalPinToInterrupt(Encoder_R_B), Encoder_RB, CHANGE);
+//Left wheel encoder pins and interrupts/
+    pinMode(Encoder_L_A, INPUT);  pinMode(Encoder_L_B, INPUT);
+    attachInterrupt(digitalPinToInterrupt(Encoder_L_A), Encoder_LA, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(Encoder_L_B), Encoder_LB, CHANGE);
+    // Right wheel encoder pins and interrupts
+    pinMode(Encoder_R_A, INPUT);  pinMode(Encoder_R_B, INPUT);
+    attachInterrupt(digitalPinToInterrupt(Encoder_R_A), Encoder_RA, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(Encoder_R_B), Encoder_RB, CHANGE);
+}
+// settings for faster ADC speed, for 7 IRs ~ 226us
+void AdcBooster() {
+    ADC->CTRLA.bit.ENABLE = 0;                      // Disable ADC
+    while (ADC->STATUS.bit.SYNCBUSY == 1);             // Wait for synchronization
+    ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 |    // Divide Clock by 128, ~31us per ADC sample
+                     ADC_CTRLB_RESSEL_12BIT;        // Result on 12 bits
+//  ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 |  // 1 sample
+//                     ADC_AVGCTRL_ADJRES(0x00ul);// Adjusting result by 0
+    ADC->SAMPCTRL.reg = 0x03;                       // Sampling Time Length = 3
+    ADC->CTRLA.bit.ENABLE = 1;                      // Enable ADC
+    while (ADC->STATUS.bit.SYNCBUSY == 1);             // Wait for synchronization
+} // AdcBooster
+// setup 1ms interrupt using timer TC3, and clock source GCLK5
+void setupTimers()
+{
+    REG_GCLK_GENCTRL = GCLK_GENCTRL_IDC |           // Set the duty cycle to 50/50 when odd division factor used
+                       GCLK_GENCTRL_GENEN |         // Enable GCLK generator
+                       GCLK_GENCTRL_SRC_DFLL48M |   // Set the 48MHz clock source
+                       GCLK_GENCTRL_ID(5);          // Select GCLK5
+//  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+    REG_GCLK_GENDIV = GCLK_GENDIV_DIV(3) |          // Divide the 48MHz clock source by divisor N=3: 48MHz/3=16MHz
+                      GCLK_GENDIV_ID(5);            // Select Generic Clock (GCLK) 5
+//  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+    // Feed GCLK5 to TCC2 and TC3
+    REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |         // Enable GCLK
+                       GCLK_CLKCTRL_GEN_GCLK5 |     // Select GCLK5
+                       GCLK_CLKCTRL_ID_TCC2_TC3;    // Feed GCLK5 to TCC2 and TC3
+//                     GCLK_CLKCTRL_ID_TC4_TC5;     // Feed GCLK5 to TC4 and TC5
+    while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+    // set TC3 in 16 bits counting mode, Divide the GCLOCK signal by 16
+    REG_TC3_CTRLA |= TC_CTRLA_MODE_COUNT16 |        // Set the counter to 16-bit mode
+                     TC_CTRLA_PRESCALER_DIV16 |     // Set prescaler to 16, 16MHz/16 = 1MHz
+                     TC_CTRLA_WAVEGEN_MFRQ |        // Set Match Frequency mode, cc0 would be period value
+                     TC_CTRLA_ENABLE;               // Enable TC3
+    while (TC3->COUNT16.STATUS.bit.SYNCBUSY);       // Wait for synchronization
+
+    // timer TC3 counts up to CC0 value to generate 1ms interrupt,
+    // this determines the frequency of the interrupt operation: Freq = 48Mhz/(N*CC0*Prescaler)
+    REG_TC3_COUNT16_CC0 =500;                    // Set the CC0 (period) register to 1000 for 1MHz clock  1kHZ  1ms
+    while (TC3->COUNT16.STATUS.bit.SYNCBUSY);     // Wait for synchronization
+
+    NVIC_SetPriority(TC3_IRQn, 1);                // Set the Nested Vector Interrupt Controller (NVIC) priority for TC3 to 3 (0 highest)
+    NVIC_EnableIRQ(TC3_IRQn);                     // Connect TC3 to Nested Vector Interrupt Controller (NVIC)
+    REG_TC3_INTFLAG |= TC_INTFLAG_MC0;            // Clears the interrupt flag
+    REG_TC3_INTENSET = TC_INTENSET_MC0;
+}
+void setupPWM() {
+    REG_GCLK_GENCTRL = GCLK_GENCTRL_IDC |           // Set the duty cycle to 50/50 HIGH/LOW
+                       GCLK_GENCTRL_GENEN |         // Enable GCLK generator
+                       GCLK_GENCTRL_SRC_DFLL48M |   // Set the 48MHz clock source
+                       GCLK_GENCTRL_ID(4);          // Select GCLK4
+    while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+    REG_GCLK_GENDIV = GCLK_GENDIV_DIV(1) |          // Divide the 48MHz clock source by divisor 1: 48MHz/1=48MHz
+                      GCLK_GENDIV_ID(4);            // Select Generic Clock (GCLK) 4
+    while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+    // Feed GCLK4 to TCC0 and TCC1
+    REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |         // Enable GCLK
+                       GCLK_CLKCTRL_GEN_GCLK4 |     // Select GCLK4
+                       GCLK_CLKCTRL_ID_TCC0_TCC1;   // Feed GCLK4 to TCC0 and TCC1
+    while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+    // Enable the port multiplexer for the digital pin D2,D3
+    PORT->Group[g_APinDescription[MOTOR_PWM_R].ulPort].PINCFG[g_APinDescription[MOTOR_PWM_R].ulPin].bit.PMUXEN = 1;
+    PORT->Group[g_APinDescription[MOTOR_PWM_L].ulPort].PINCFG[g_APinDescription[MOTOR_PWM_L].ulPin].bit.PMUXEN = 1;
+
+    // Connect the TCC0 timer to digital output D2,D3 - port pins are paired odd PMUO and even PMUXE
+    // F specify the timers: TCC0
+    PORT->Group[g_APinDescription[MOTOR_PWM_R].ulPort].PMUX[g_APinDescription[MOTOR_PWM_R].ulPin >> 1].reg = PORT_PMUX_PMUXO_F;
+    PORT->Group[g_APinDescription[MOTOR_PWM_L].ulPort].PMUX[g_APinDescription[MOTOR_PWM_L].ulPin >> 1].reg = PORT_PMUX_PMUXE_F;
+
+    REG_TCC0_WAVE |= TCC_WAVE_WAVEGEN_NPWM;         // Setup Single slope PWM on TCC0
+    while (TCC0->SYNCBUSY.bit.WAVE);                // Wait for synchronization
+
+    // Each timer counts up to a maximum or TOP value set by the PER register,
+    // this determines the frequency of the PWM operation:
+    REG_TCC0_PER = 4000;                            // Set the frequency of the PWM on TCC0 to 12kHz
+    while (TCC0->SYNCBUSY.bit.PER);                 // Wait for synchronization
+
+    // Set the PWM signal to output 0% duty cycle
+    REG_TCC0_CC3 = 0;                               // TCC0 CC3 - on D2
+    while (TCC0->SYNCBUSY.bit.CC3);                 // Wait for synchronization
+    REG_TCC0_CC0 = 0;                               // TCC0 CC0 - on D3
+    while (TCC0->SYNCBUSY.bit.CC0);                 // Wait for synchronization
+    // Divide the 48MHz signal by 1 giving 48MHz (20.83ns) TCC0 timer tick and enable the outputs
+    REG_TCC0_CTRLA |= TCC_CTRLA_PRESCALER_DIV1 |    // Divide GCLK4 by 1
+                      TCC_CTRLA_ENABLE;             // Enable the TCC0 output
+    while (TCC0->SYNCBUSY.bit.ENABLE);              // Wait for synchronization
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,30 @@
 #include <Init_car_index.h>
 void setup()
 {
+    AdcBooster();
     Init_Peripherals();
+    setupTimers();
+    setupPWM();
     SerialUSB.begin(115200);
 }
 
 void loop()
 {
-    digitalWrite(Buzzer_PIN,ON);
-    digitalWrite(IR3_OUT,HIGH);
-    IRsensors[3]=analogRead(IR3);
-    digitalWrite(IR3_OUT,LOW);
-    digitalWrite(IR0_4_OUT,HIGH);
-    IRsensors[0]=analogRead(IR_hint_L);
-    IRsensors[4]=analogRead(IR4);
-    digitalWrite(IR0_4_OUT,LOW);
-    digitalWrite(IR2_6_OUT,HIGH);
-    IRsensors[6]=analogRead(IR_StartStop_R);
-    IRsensors[2]=analogRead(IR2);
-    digitalWrite(IR2_6_OUT,LOW);
-    digitalWrite(IR1_5_OUT,HIGH);
-    IRsensors[1]=analogRead(IR1);
-    IRsensors[5]=analogRead(IR5);
-    digitalWrite(IR1_5_OUT,LOW);
-    digitalWrite(Buzzer_PIN,OFF);
-    for (int i = 0; i < 7; ++i) {
-    SerialUSB.print(IRsensors[i]);
-    SerialUSB.print("\t");
-    }
-    SerialUSB.println();
+
+//    for (int i = 0; i < 7; ++i) {
+//    SerialUSB.print(IRsensors[i]);
+//    SerialUSB.print("\t");
+//    }
+//    SerialUSB.println();
+
+    SerialUSB.println(Vcount_R);
+}
+// TC3 Interrupt Service Routine
+void TC3_Handler()
+{
+    checkButton();
+    READ_QEI();
+    QEI_filter();
+    // clear the interrupt flag
+    REG_TC3_INTFLAG |= TC_INTFLAG_MC0;
 }
